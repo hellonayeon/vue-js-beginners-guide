7fca590045bbcd50f3fcfd0bf65650ec567a9737
hellonayeon <hellonykwon@gmail.com>
2023-05-05 12:44:22 +0900

    feat: 싱글 파일 컴포넌트 소개 및 여태까지 배운 내용 적용하는 방법

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,28 @@
+var appHeader = {
+	template: '<div>header</div>',
+	methods: {
+		addNum: function() {
+
+		}
+	}
+}
+
+
+<template>
+	<div>header</div>
+</template>
+
+<script>
+export default { // ES6의 Modules와 관련된 문법
+	// Javascript - 인스턴스 옵션
+	methods: {
+		addNum: function() {
+			
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
